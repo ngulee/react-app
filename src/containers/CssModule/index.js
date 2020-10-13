56dde5c451642b6d8400b92496e6d36f6838ba2f
lizuoxing <lizuoxing4534@gmail.com>
2020-10-13 14:42:21 +0800

    feat： css module

--- src/containers/CssModule/index.js
+++ src/containers/CssModule/index.js
@@ -1,16 +1,22 @@
 import React, { Component } from 'react';
+import cls from 'classnames';
 
-import listStyle from './style/list.module.css';
+import listStyle from './style/list.module.scss';
+
+console.log('listStyle:', listStyle)
 
 const list = ('name').split('');
+
+const { listItem, listItemActive } = listStyle;
 
 const UlList = () => {
   return (
     <ul className={listStyle.list}>
       {
         list.map((item) => {
+          const itemCls = cls(listItem, listItemActive);
           return (
-          <li key={item}>{item}</li>
+          <li key={item} className={itemCls}>{item}</li>
           )
         })
       }
